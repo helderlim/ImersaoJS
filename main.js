c8bca5b7ba528b6f8d6a923d1250c9bcc1650fb6
helderlim <helderfelima16@gmail.com>
2021-03-25 23:14:59 -0300

    feat: calculadora e condicionais Aula2

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-var valorEmDolarTexto = prompt("Qual o valor em dolar que voce quer converter? ")
 
-var valorEmDolarNumero = parseFloat(valorEmDolarTexto)
+var primeiroValor = parseInt(prompt('Digite o primeiro valor: '))
+var segundoValor = parseInt(prompt('Digite o segundo valor: ')) 
 
-var valorEmReal = valorEmDolarNumero * 5.50
-var valoremRealDecimal = valorEmReal.toFixed(2)
-alert(valoremRealDecimal)
+
+
+var operacao = prompt("Digite 1- divisão, 2- multiplicação, 3- somar, 4- subtrair: ")
+
+if (operacao == 1) {
+    var resultado = primeiroValor / segundoValor
+    document.write("<h2>" + primeiroValor + ' / ' + segundoValor + ' = ' + resultado + "</h2>")
+}else if( operacao == 2) {
+    var resultado = primeiroValor * segundoValor
+    document.write("<h2>" + primeiroValor + ' * ' + segundoValor + ' = ' + resultado + "</h2>")
+}else if( operacao == 3) {
+    var resultado = primeiroValor + segundoValor
+    document.write("<h2>" + primeiroValor + ' + ' + segundoValor + ' = ' + resultado + "</h2>")
+}else if( operacao == 4){
+    var resultado = primeiroValor - segundoValor
+    document.write("<h2>" + primeiroValor + ' - ' + segundoValor + ' = ' + resultado + "</h2>")
+}else{
+    document.write('opção invalida')
+}
+
+
