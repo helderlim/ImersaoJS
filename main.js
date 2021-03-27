e0b19fb151df99de0dd42e1e4d8b47384b91599d
helderlim <helderfelima16@gmail.com>
2021-03-26 22:02:29 -0300

    feat: Mentalista e Lacos

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
-
-var primeiroValor = parseInt(prompt('Digite o primeiro valor: '))
-var segundoValor = parseInt(prompt('Digite o segundo valor: ')) 
-
+var numeroSecreto = parseInt(Math.random() * 10)
+var tentativas = 3
 
 
-var operacao = prompt("Digite 1- divisão, 2- multiplicação, 3- somar, 4- subtrair: ")
+while (tentativas > 0){
+    var chute = parseInt(prompt("Digte um numero entre 0 e 10 "))
 
-if (operacao == 1) {
-    var resultado = primeiroValor / segundoValor
-    document.write("<h2>" + primeiroValor + ' / ' + segundoValor + ' = ' + resultado + "</h2>")
-}else if( operacao == 2) {
-    var resultado = primeiroValor * segundoValor
-    document.write("<h2>" + primeiroValor + ' * ' + segundoValor + ' = ' + resultado + "</h2>")
-}else if( operacao == 3) {
-    var resultado = primeiroValor + segundoValor
-    document.write("<h2>" + primeiroValor + ' + ' + segundoValor + ' = ' + resultado + "</h2>")
-}else if( operacao == 4){
-    var resultado = primeiroValor - segundoValor
-    document.write("<h2>" + primeiroValor + ' - ' + segundoValor + ' = ' + resultado + "</h2>")
-}else{
-    document.write('opção invalida')
+    if (numeroSecreto == chute){
+        console.log('acertou')
+        break;
+    }else if (chute > numeroSecreto){
+        console.log("O numero secreto é menor ");
+        tentativas--;
+    }else if (chute < numeroSecreto){
+        console.log("o numero secreto é maior ");
+        tentativas--;
+    }
+    
 }
 
-
+if( chute != numeroSecreto){
+    console.log("Suas tentativas acabaram o numero secreto era: " + numeroSecreto);
+}
